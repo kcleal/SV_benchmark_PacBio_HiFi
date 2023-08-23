b24ceb51c69514570572bf13a26bc8b470b2869a
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-08-23 11:56:14 +0100

    Update plot_benchmark.py

--- plot_benchmark.py
+++ plot_benchmark.py
@@ -16,7 +16,7 @@ for caller in ('sniffles', 'cuteSV', 'delly', 'dysgu', 'NGSEP.vcf_SVsLongReads',
         data.append(d)
     with open(f'truvari_all_{caller}/summary.json', 'r') as f:
          dta = json.load(f)
-    d = {'caller': caller.replace('.vcf_SVsLongReads',''), 'sample': 'all', 'TP': None}
+    d = {'caller': caller.replace('.vcf_SVsLongReads',''), 'sample': 'merged', 'TP': None}
     d.update({k: v for k, v in dta.items() if k in ('precision', 'recall', 'f1', 'gt_concordance', 'TP-base', 'FP', 'FN')})
     d['TP'] = d['TP-base']
     del d['TP-base']
@@ -26,12 +26,12 @@ for caller in ('sniffles', 'cuteSV', 'delly', 'dysgu', 'NGSEP.vcf_SVsLongReads',
 df = pd.DataFrame().from_records(data)
 cols = ['caller', 'TP', 'FP', 'FN', 'precision', 'recall', 'f1', 'gt_concordance']
 print('Merged runs:')
-print(df[df['sample'] == 'all'][cols].round(4).to_markdown(index=False))
+print(df[df['sample'] == 'merged'][cols].round(4).to_markdown(index=False))
 print('Single run average:')
-print(df[df['sample'] != 'all'][cols].groupby('caller', sort=False).mean().round(3).to_markdown())
+print(df[df['sample'] != 'merged'][cols].groupby('caller', sort=False).mean().round(3).to_markdown())
 print('Single runs:')
 cols.append('depth')
-print(df[df['sample'] != 'all'][cols].round(4).to_markdown(index=False))
+print(df[df['sample'] != 'merged'][cols].round(4).to_markdown(index=False))
 
 sns.set_style("whitegrid")
 plt.figure(figsize=(8, 4))
@@ -40,10 +40,12 @@ ax = sns.scatterplot(df, x="recall", y="precision", hue="caller", style="sample"
 ax.legend(bbox_to_anchor=(1.03, 0.5), loc='center left')
 plt.savefig("benchmark_result.png")
 
+
 plt.figure(figsize=(8, 4))
 plt.subplots_adjust(right=0.75)
-ax = sns.scatterplot(df[df['sample'] != 'all'], x="depth", y="f1", hue="caller", style="sample", s=100)
+ax = sns.scatterplot(df[df['sample'] != 'merged'], x="depth", y="f1", hue="caller", style="sample", s=100)
 ax.legend(bbox_to_anchor=(1.03, 0.5), loc='center left')
 plt.ylim(0.86, 0.96)
 plt.savefig("benchmark_result_f1.png")
 plt.show()
+
